9b0a4d9417d42fae1b85ca58cb884f0cb8ffa79b
yuusanx3 <tomokiyuusuke0726@gmail.com>
2023-10-29 15:34:56 +0900

    henkou

--- data_arrange.py
+++ data_arrange.py
@@ -6,13 +6,14 @@
 # v0.x.x.x : YYYY/MM/DD 作成者 新規作成
 ########################################################################################################################
 import os
+import sys
 import traceback
 import logging
 import pandas as pd
 import common
 
 # 設定ファイルパス
-CONFIG = "main.ini"
+CONFIG = "data_arrange.ini"
 
 class CommonException(Exception):
     """
@@ -29,6 +30,24 @@ class Const:
     # ログ文字列
     LOG_STR_UNEXPECTED_ERROR = "意図しない例外が発生しました。システム担当者に問い合わせてください。"
 
+    INPUT = None
+    OUTPUT = None
+
+    FILE_TYPE = {
+        "A01-1_（A01ファイルに読み込まれるデータ）.xlsx": "人口",
+        "A02-1_（A02ファイルに読み込まれるデータ）.xlsx": "産業",
+        "D01-1_（D01ファイルに読み込まれるデータ）.xlsx": "人口増減",
+        "D02-1_（D02ファイルに読み込まれるデータ）.xlsx": "製造業",
+        "D03-1_（D03ファイルに読み込まれるデータ）.xlsx": "小売業",
+        "D04-1_（D03ファイルに読み込まれるデータ）.xlsx": "農業",
+        "D05-1_（D03ファイルに読み込まれるデータ）.xlsx": "林業",
+        "D06-1_（D03ファイルに読み込まれるデータ）.xlsx": "水産業",
+        "D07-1_（D03ファイルに読み込まれるデータ）.xlsx": "観光",
+        "D08-1_（D03ファイルに読み込まれるデータ）.xlsx": "雇用",
+        "D09-1_（D03ファイルに読み込まれるデータ）.xlsx": "医療・福祉",
+        "D10-1_（D03ファイルに読み込まれるデータ）.xlsx": "地方財政",
+    }
+
     @classmethod
     def set_all(cls,config) -> None:
         """
@@ -43,24 +62,8 @@ class Const:
         config
             設定ファイル
         """
-        pass
-
-class DbSqlite(common.SqliteDB):
-    """
-    SqliteDBを実行するクラス
-    
-    あくまでも例です。
-
-    Parameters
-    ----------
-    database
-        データベース名
-    log
-        ログクラス
-    """
-    def __init__(self,database,log) -> None:
-        # PostgreDBクラスの設定
-        super().__init__(database,log=log)
+        cls.INPUT = config["CONST"]["INPUT"]
+        cls.OUTPUT = config["CONST"]["OUTPUT"]
 
 class Main():
     """
@@ -77,8 +80,6 @@ class Main():
         # ログクラスの設定
         self.log = common.Log(log_path,logging.DEBUG)
         self.logger = self.log.logger
-        # SqliteDBクラスの設定
-        self.db = DbSqlite(self.config["DATABASE"]["DATABASE"],self.log)
 
     def main(self) -> None:
         """
@@ -90,10 +91,29 @@ class Main():
             # 開始ログ出力
             self.log.info_start()
 
-            
+            # 引数
+            if 3 != len(sys.argv):
+                raise CommonException("引数が異なります。")
+
+            # 都道府県
+            pref = sys.argv[1]
+            # 都市
+            city = sys.argv[2]
+
+            # ディレクトリ一覧
+            files = os.listdir(Const.INPUT)
+            for file in files:
+                # 移動元フルパス
+                file_from = os.path.join(Const.INPUT,file)
+                # ファイル種別
+                file_type = Const.FILE_TYPE[file]
+                # 移動先フルパス
+                file_to = os.path.join(Const.OUTPUT,f"{pref}_{city}_{file_type}.xlsx")
+                os.rename(file_from,file_to)
             
         except CommonException as e:
             self.logger.error(str(e))
+            print(str(e))
         except Exception:
             self.logger.error(traceback.format_exc())
             self.logger.error(Const.LOG_STR_UNEXPECTED_ERROR)
